b2f8ba93f8aa2805e8b1b6d14fbbc28bbaa0fc9e
Johns-Costa <>
2023-09-10 13:09:30 +0000

    create modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -193,3 +193,30 @@ patternGrid2.addEventListener('click', event => {
 document.getElementById('generate-button2').addEventListener('click', newGame2);
 
 document.getElementById('check2').addEventListener('click', checkPattern2);
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("rules-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
